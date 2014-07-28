8e1807184f42baf7fd23912d7d3bc97f86288f4c
Lasse Öörni <loorni@gmail.com>
2014-07-29 02:24:49 +0300

    Return a writable documents directory on iOS. Contributed by att.

--- src/video/uikit/SDL_uikitappdelegate.m
+++ src/video/uikit/SDL_uikitappdelegate.m
@@ -45,8 +45,9 @@ static int forward_argc;
 static char **forward_argv;
 static int exit_status;
 
-// Urho3D: added variable
+// Urho3D: added variables
 const char* resource_dir = 0;
+const char* documents_dir = 0;
 
 int main(int argc, char **argv)
 {
@@ -94,6 +95,22 @@ const char* SDL_IOS_GetResourceDir()
     }
     
     return resource_dir;
+}
+
+// Urho3D: added function
+const char* SDL_IOS_GetDocumentsDir()
+{
+    if (!documents_dir)
+    {
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSString *basePath = ([paths count] > 0) ? [paths objectAtIndex:0] : nil;
+        
+        const char *temp = [basePath UTF8String];
+        documents_dir = malloc(strlen(temp) + 1);
+        strcpy(documents_dir, temp);
+    }
+    
+    return documents_dir;
 }
 
 static void
