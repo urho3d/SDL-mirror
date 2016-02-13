495d6da9ac67068d33993b44aed1bf0f56e3800d
Yao Wei Tjong 姚伟忠 <weitjong@gmail.com>
2016-02-13 13:28:50 +0800

    Utilize new SDLActivity's get arguments feature.

--- src/main/android/SDL_android_main.c
+++ src/main/android/SDL_android_main.c
@@ -41,12 +41,9 @@ JNIEXPORT int JNICALL Java_org_libsdl_app_SDLActivity_nativeInit(JNIEnv* env, jc
     /* Prepare the arguments. */
 
     len = (*env)->GetArrayLength(env, array);
-    argv = SDL_stack_alloc(char*, 1 + len + 1);
+    argv = SDL_stack_alloc(char*, len + 1);
     argc = 0;
-    /* Use the name "app_process" so PHYSFS_platformCalcBaseDir() works.
-       https://bitbucket.org/MartinFelis/love-android-sdl2/issue/23/release-build-crash-on-start
-     */
-    argv[argc++] = SDL_strdup("app_process");
+    // Urho3D: avoid hard-coding the "app_process" as the first argument
     for (i = 0; i < len; ++i) {
         const char* utf;
         char* arg = NULL;
